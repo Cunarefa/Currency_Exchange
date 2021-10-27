9ae4e2ff41c4b76b3191d9c972d0fcff0655e6f2
Yevgen <yevgen.yelik@gmail.com>
2021-10-27 14:54:51 +0300

    fix model fields order

--- application/handlers/alphavantage_model_fields.py
+++ application/handlers/alphavantage_model_fields.py
@@ -14,8 +14,8 @@ class ModelFieldsHandler():
 
             model_fields['from_currency'] = realtime_data["1. From_Currency Code"]
             model_fields['to_currency'] = realtime_data["3. To_Currency Code"]
-            model_fields['bid_price'] = realtime_data["9. Ask Price"]
-            model_fields['ask_price'] = realtime_data["8. Bid Price"]
+            model_fields['bid_price'] = realtime_data["8. Bid Price"]
+            model_fields['ask_price'] = realtime_data["9. Ask Price"]
             model_fields['exchange_rate'] = realtime_data["5. Exchange Rate"]
             return model_fields
         except Exception as err:
