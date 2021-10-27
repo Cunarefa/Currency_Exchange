2e9f9d2780017066607bc3894835d69b973ac464
Yevgen <yevgen.yelik@gmail.com>
2021-10-27 19:04:16 +0300

    delete url_handler method

--- application/handlers/__init__.py
+++ application/handlers/__init__.py
@@ -1,12 +1,2 @@
-import requests
-
-
-def url_handler(url):
-    try:
-        data = requests.get(url)
-        return data
-    except Exception as err:
-        return err
-
 
 DEFAULT_VALUES = dict(from_currency='BTC', to_currency='USD')
