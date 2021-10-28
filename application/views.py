53e2b2e6354b5fc548553906bd32f2d9122b7a0b
Yevgen <yevgen.yelik@gmail.com>
2021-10-28 15:11:02 +0300

    add create method

--- application/views.py
+++ application/views.py
@@ -1,9 +1,12 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
+from CurrencyExchange.settings import DOMAIN
 from application.models import Currency
 from application.serializers import CurrencySerializer
+from application.tasks import get_exchange_rate_task
 
 
 class CurrencyViewSet(viewsets.ModelViewSet):
@@ -12,3 +15,6 @@ class CurrencyViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
     authentication_classes = (TokenAuthentication,)
 
+    def create(self, request, *args, **kwargs):
+        currency = get_exchange_rate_task.delay(DOMAIN)
+        return Response(currency.get())
